f191ec59746ae95a26dec4e822f0099420ea2088
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-14 17:14:22 -0800

    0.1.6

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,8 @@
-const fs = require('fs-extra')
 const path = require('path')
+const fs = require('fs')
 
 const from = path.join(__dirname, '..', 'dll', process.arch, 'InteractiveNotifications.dll')
 const to = path.join(__dirname, '..', 'build', 'Release', 'InteractiveNotifications.dll')
 
-fs.copySync(from, to)
+fs.createReadStream(from).pipe(fs.createWriteStream(to))
+
