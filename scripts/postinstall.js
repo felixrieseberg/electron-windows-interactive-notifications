3965d53e1af0f759dfb07c270eae9fd851ae012b
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-14 17:14:12 -0800

    :wrench: Fix postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const from = path.join(__dirname, 'dll', process.arch, 'InteractiveNotifications.dll')
-const to = path.join(__dirname, 'build', 'Release')
+const from = path.join(__dirname, '..', 'dll', process.arch, 'InteractiveNotifications.dll')
+const to = path.join(__dirname, '..', 'build', 'Release', 'InteractiveNotifications.dll')
 
 fs.copySync(from, to)
