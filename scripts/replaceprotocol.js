f267608629ed1f62e7877ccc300220e1131d1ac0
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-27 16:54:59 -0800

    :package: Fetch protcol from package

--- scripts/replaceprotocol.js
+++ scripts/replaceprotocol.js
@@ -8,8 +8,46 @@ const {replaceSync} = require('./utils')
 
 const DEFINED_PROTOCOL = /slack:\/\//gi
 
+/**
+ * Attempts to get the protocol from the environment
+ *
+ * @returns {string|null} protocol
+ */
+function getProtocolFromEnvironment () {
+  return process.env.TOAST_ACTIVATOR_PROTOCOL || null
+}
+
+/**
+ * Attempts to get the protocol from the package.json
+ * in the current working directory (usually the app for
+ * which `npm intall` was run)
+ *
+ * @returns {string|null} protocol
+ */
+function getProtocolFromPackage () {
+  let appPackage = getAppPackage() || {}
+  let interativeNotifications = appPackage['interactive-notifications'] || {}
+
+  return interativeNotifications.protocol || null
+}
+
+/**
+ * Attempts to get the protocol to use, using the following
+ * methods:
+ *  (1) From the environment
+ *  (2) From the package.json in the current working directory
+ *
+ * @returns {string}
+ */
+function getProtocol () {
+  return getProtocolFromEnvironment() || getProtocolFromPackage()
+}
+
+/**
+ * Replaces the protocol in the C++ source files
+ */
 function replaceProtocol () {
-  const newProtocol = process.env.TOAST_ACTIVATOR_PROTOCOL
+  const newProtocol = getProtocol()
   const cppFile = path.join(__dirname, '..', 'InteractiveNotifications', 'InteractiveNotifications.cpp')
 
   console.log(`\n##### ToastActivator Protocol #############################`)
