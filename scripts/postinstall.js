add93423bf68ba45ec604c1a0df4cde345faa0ca
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-27 19:11:00 -0800

    :wrench: Fix x64 error

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 const releaseKey = process.arch === 'x64' ? 'x64/Release' : 'Release'
-const from = path.join(__dirname, '..', 'dll', releaseKey, 'InteractiveNotifications.dll')
+const from = path.join(__dirname, '..', releaseKey, 'InteractiveNotifications.dll')
 const to = path.join(__dirname, '..', 'build', 'Release', 'InteractiveNotifications.dll')
 
 fs.createReadStream(from).pipe(fs.createWriteStream(to))
