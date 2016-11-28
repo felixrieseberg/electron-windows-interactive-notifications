f737df092b4662581ea8b181c703c7d591828576
Felix Rieseberg <felix@felixrieseberg.com>
2016-11-27 16:51:24 -0800

    :package: Fetch CLSID from package

--- scripts/replaceguid.js
+++ scripts/replaceguid.js
@@ -5,44 +5,77 @@
 const path = require('path')
 const fs = require('fs')
 const crypto = require('crypto')
-const {replaceSync} = require('./utils')
+const {replaceSync, getAppPackage} = require('./utils')
 
 const DEFINED_CLSID = /A23D2B18-8DD7-403A-B9B7-152B40A1478C/gi
 
-function getClsid () {
-  if (process.env.TOAST_ACTIVATOR_CLSID) return process.env.TOAST_ACTIVATOR_CLSID
+/**
+ * Attempts to fetch the CLSID from the environment
+ *
+ * @returns {string|null} CLSID
+ */
+function getClsidFromEnvironment () {
+  return process.env.TOAST_ACTIVATOR_CLSID || null
+}
 
-  const rnds = crypto.randomBytes(16)
+/**
+ * Attempts to fetch the CLSID using a package.json present
+ * in the current working directory (usually the application
+ * for which `npm install` was run)
+ *
+ * @returns {string|null} CLSID
+ */
+function getClsidFromPackage () {
+  let appPackage = getAppPackage() || {}
+  let interativeNotifications = appPackage['interactive-notifications'] || {}
+
+  return interativeNotifications['toast-activator-clsid'] || null
+}
+
+/**
+ * Randomly generates a CLSID
+ *
+ * @returns {string} CLSID
+ */
+function generateClsid () {
+  let rnds = crypto.randomBytes(16)
+  let bth = []
+  let i = 0
 
   // Set bits for version and `clock_seq_hi_and_reserved`
   rnds[6] = (rnds[6] & 0x0f) | 0x40
   rnds[8] = (rnds[8] & 0x3f) | 0x80
 
-  return buff_to_string(rnds)
-}
-
-function buff_to_string (buf) {
-  const i = 0
-  const _byteToHex = []
-  const _hexToByte = {}
-
-  for (let ii = 0; i < 256; ++i) {
-    _byteToHex[i] = (i + 0x100).toString(16).substr(1)
-    _hexToByte[_byteToHex[i]] = i
+  for (let i = 0; i < 256; ++i) {
+    bth[i] = (i + 0x100).toString(16).substr(1);
   }
 
-  const bth = _byteToHex
-
-  return bth[buf[i++]] + bth[buf[i++]] +
-    bth[buf[i++]] + bth[buf[i++]] + '-' +
-    bth[buf[i++]] + bth[buf[i++]] + '-' +
-    bth[buf[i++]] + bth[buf[i++]] + '-' +
-    bth[buf[i++]] + bth[buf[i++]] + '-' +
-    bth[buf[i++]] + bth[buf[i++]] +
-    bth[buf[i++]] + bth[buf[i++]] +
-    bth[buf[i++]] + bth[buf[i++]]
+  return  bth[rnds[i++]] + bth[rnds[i++]] +
+          bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+          bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+          bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+          bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+          bth[rnds[i++]] + bth[rnds[i++]] +
+          bth[rnds[i++]] + bth[rnds[i++]] +
+          bth[rnds[i++]] + bth[rnds[i++]]
 }
 
+/**
+ * Returns a CLSID, using the following methods:
+ *
+ *  (1) Using an environment variable
+ *  (2) Using a package.json present in the current working directory
+ *  (3) Using a random CLSID generator
+ *
+ * @returns {string} CLSID
+ */
+function getClsid () {
+  return getClsidFromEnvironment() || getClsidFromPackage() || generateClsid()
+}
+
+/**
+ * Replaces the CLSID in the C++ source files
+ */
 function replaceCLSID () {
   const newClsid = getClsid()
   const cppFile = path.join(__dirname, '..', 'InteractiveNotifications', 'InteractiveNotifications.cpp')
@@ -55,4 +88,6 @@ function replaceCLSID () {
   replaceSync(cppFile, DEFINED_CLSID, newClsid)
 }
 
-module.exports = replaceCLSID
+console.log(getClsid())
+
+//module.exports = replaceCLSID
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 function replaceSync (file, find, replace) {
   const contents = fs.readFileSync(file, 'utf8')
@@ -10,4 +11,18 @@ function replaceSync (file, find, replace) {
   return true
 }
 
-module.exports = { replaceSync }
+function getAppPackage () {
+  let package = null
+
+  if (process.cwd()) {
+    try {
+      package = require(path.join(process.cwd(), 'package.json'))
+    } catch (e) {
+      // no-op
+    }
+  }
+
+  return package
+}
+
+module.exports = { replaceSync, getAppPackage }
